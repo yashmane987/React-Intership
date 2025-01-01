4d3383dcea04f33c5bc0c3b54c318617f01d4d3c
yashmane987 <yashmane4045@gmail.com>
2025-01-01 11:16:16 +0530

    react-frontend

---
+++ javascript/concept.js
@@ -0,0 +1,21 @@
+//Array
+
+const arrayFruits =['apple','banana','cherry']
+const arrayNumber =[10,20,30]
+console.log(arrayFruits)
+console.log(arrayNumber)
+
+//object
+
+let person={
+    name: "Yash",
+    age: "23",
+    clg: "SGM"
+}
+console.log(person,"All objects")
+console.log(person.name,"Only name")
+
+//String
+
+let name = "yash technology"
+console.log(name,"string 1")
