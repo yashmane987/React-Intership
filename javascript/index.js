85b075e0da630f72d0422f963c7000f239c757ec
yashmane987 <yashmane4045@gmail.com>
2025-01-01 12:43:53 +0530

    js changes

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,25 @@
-console.log("Test 1")
+//Var = old way
+
+var x = 10; //globally scope
+if(true){
+    var x = 20
+    console.log(x)
+}
+
+console.log(x) //20
+
+//let = morden Way
+ 
+let y = 20;
+if(true){
+    let y = 30
+    console.log(y) //30
+}
+
+console.log(y)
+
+
+//const do not change value
+
+const z = 10;
+console.log(z);
