9c523f6d4e7c03b1ad20a741e72c7e2f65a9bb50
yashmane987 <yashmane4045@gmail.com>
2025-01-02 20:15:40 +0530

    react project create

---
+++ Reactjs/project1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './new.css';
+
+function App() {
+  return (
+    <div className="new">
+     <h1><span>Y</span>ash<span>M</span></h1>
+     <div className="hero">
+<h1>I'm Yash</h1>
+<p>I am learning react js for create frontend website and UI </p>
+     </div>
+    </div>
+  );
+}
+
+export default App;
