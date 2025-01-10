45909413d18e0e584e29c479add15e8d649cfe8c
yashmane987 <yashmane4045@gmail.com>
2025-01-10 12:29:29 +0530

    learn navigate one url to another

--- Reactjs/project1/src/index.js
+++ Reactjs/project1/src/index.js
@@ -7,16 +7,16 @@ import { BrowserRouter ,Routes,Route} from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import AboutUsPage from './Pages/AboutUsPage';
 import ContactUs from './Pages/ContactUs';
+import Service from './Pages/Service';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
-
-      <Route path="home" element={<HomePage/>} />
+      <Route path="/" element={<HomePage/>} />
       <Route path="aboutus" element={<AboutUsPage/>} />
       <Route path="contactus" element={<ContactUs/>} />
-      
+      <Route path='service' element={<Service/>} />
       </Routes>
       </BrowserRouter>
   </React.StrictMode>
