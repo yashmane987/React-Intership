8b4ee347d8f60ce090d695486c5b77b99c95a145
yashmane987 <yashmane4045@gmail.com>
2025-01-11 01:06:06 +0530

    learn  img and text tag

--- Reactjs/project1/src/Pages/AboutUsPage.jsx
+++ Reactjs/project1/src/Pages/AboutUsPage.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './aboutus.css'
+import img1 from "../images/img1.png";
+
 const AboutUsPage = () => {
   return (
      <div className="hero">
@@ -11,8 +14,23 @@ const AboutUsPage = () => {
         <li><Link to="/contactus">ContactUs</Link></li> 
         </ul>
     </nav>
-    <h2>This is AboutUs Page</h2>
+    <div className="container">
+      <h2>AboutUs</h2>
     </div>
+    <div className="main-div">
+        <div className="row">
+          <div className="col-6">
+            <img src={img1} className="img-box" alt='aboutus' />  
+          </div>
+          <div className="col-6">
+          <h1>I'm <span>Yash</span></h1>
+
+            <p> MERN Stack Developer with an entry-level foundation in MongoDB, Express.js, React.js, and Node.js.
+             Passionate about building efficient and scalable web applications with a keen interest in front-end and</p> 
+          </div>
+        </div>
+        </div>
+        </div>
   )
 }
 
